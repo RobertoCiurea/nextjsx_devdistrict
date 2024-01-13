a3658fb43cec7a2ace0badab09cf6ff28e4ff366
RobertoCiurea <robertociureaionut@gmail.com>
2024-01-13 14:26:44 +0200

    initialized main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "./utils/auth";
+import { getSession } from "@/app/utils/getSession";
 import Image from "next/image";
+import Banner from "@/public/images/devdistrict_banner.png";
+import { signOut } from "next-auth/react";
 export default async function Home() {
-  const session = await getServerSession(authOptions);
-  console.log(session?.user?.image);
+  const session = await getSession();
+  console.log(session);
+  // console.log(session?.user?.image);
   return (
     <>
       <h1>Home page</h1>
+      <h1>Hellog {session?.user?.name}</h1>
     </>
   );
 }

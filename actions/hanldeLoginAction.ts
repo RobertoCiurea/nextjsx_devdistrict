006dd074a18c5ae53a2cec9eee623ca622afcc2f
RobertoCiurea <robertociureaionut@gmail.com>
2024-02-10 20:25:30 +0200

    modified action to give feedback to the user

--- actions/hanldeLoginAction.ts
+++ actions/hanldeLoginAction.ts
@@ -1,16 +1,18 @@
 import { signIn } from "next-auth/react";
 
-export const handleLoginAction = async (formData: FormData) => {
+export const handleLoginAction = async (prevState: any, formData: FormData) => {
   try {
     const data = {
       email: formData.get("email"),
       password: formData.get("password"),
     };
     console.log(data);
-    signIn("credentials", {
+    const res = await signIn("credentials", {
       ...data,
       callbackUrl: "/",
+      redirect: false,
     });
+    return res;
   } catch (error) {
     console.log(error);
   }

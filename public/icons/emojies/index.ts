0541c7d3c1b90410e0d5cc6ca01f613671d453e6
RobertoCiurea <robertociureaionut@gmail.com>
2024-02-21 19:22:00 +0200

    added emoji icons

---
+++ public/icons/emojies/index.ts
@@ -0,0 +1,19 @@
+import AnrgyEmoji from "./angry_emoji.svg";
+import AnrgyEmojiFcoused from "./angry_emoji_focused.svg";
+import DeadEmoji from "./dead_emoji.svg";
+import DeadEmojiFocused from "./dead_emoji_focused.svg";
+import DisturbedEmoji from "./disturbed_emoji.svg";
+import DisturbedEmojiFocused from "./disturbed_emoji_focused.svg";
+import DissapointedEmoji from "./dissapointed_emoji.svg";
+import DissapointedEmojiFocused from "./dissapointed_emoji_focused.svg";
+
+export {
+  AnrgyEmoji,
+  AnrgyEmojiFcoused,
+  DeadEmoji,
+  DeadEmojiFocused,
+  DisturbedEmoji,
+  DisturbedEmojiFocused,
+  DissapointedEmoji,
+  DissapointedEmojiFocused,
+};

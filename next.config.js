2c4f1a35a5e9740e7c249a93d15cf65af59dd7f9
RobertoCiurea <robertociureaionut@gmail.com>
2024-03-13 17:29:06 +0200

    updated config

--- next.config.js
+++ next.config.js
@@ -11,6 +11,12 @@ const nextConfig = {
       },
       {
         protocol: "https",
+        hostname: "api.dicebear.com",
+        port: "",
+        pathname: "/7.x/avataaars-neutral/**",
+      },
+      {
+        protocol: "https",
         hostname: "avatars.githubusercontent.com",
         port: "",
         pathname: "/u/**",

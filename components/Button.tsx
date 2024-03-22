541dbe1000223b91ffc8570d7eeb8cdc0bd31741
RobertoCiurea <robertociureaionut@gmail.com>
2024-03-22 19:12:11 +0200

    added more properties

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,20 +5,43 @@ type ButtonType = {
   image?: SVGProps<SVGSVGElement> | string | any;
   buttonStyles: string;
   styles: string;
+  imgWidth?: number;
+  imgHeight?: number;
+  buttonType?: string | any;
+  handler?: () => void | any;
 };
-const Button = ({ title, image, buttonStyles, styles }: ButtonType) => {
+const Button = ({
+  title,
+  image,
+  buttonStyles,
+  imgWidth,
+  imgHeight,
+  styles,
+  buttonType,
+  handler,
+}: ButtonType) => {
   if (image) {
     return (
       <div
         className={` cursor-pointer w-full  sm:text-base sm:px-3 sm:py-3 sm:w-auto py-2 px-1 flex justify-center gap-3  transition-colors ${styles} `}
+        onClick={handler}
       >
-        <Image src={image} alt={title} />
+        <Image src={image} alt={title} width={imgWidth} height={imgHeight} />
 
-        <button className={buttonStyles}>{title}</button>
+        <button
+          className={buttonStyles}
+          type={buttonType ? buttonType : "button"}
+        >
+          {title}
+        </button>
       </div>
     );
   } else {
-    return <button className={buttonStyles}>{title}</button>;
+    return (
+      <button className={buttonStyles} onClick={handler}>
+        {title}
+      </button>
+    );
   }
 };
 

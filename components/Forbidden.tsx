ce9c9a051bf67e5933d7d13b50d49538aa247386
RobertoCiurea <robertociureaionut@gmail.com>
2024-03-22 19:15:14 +0200

    added more gifs

--- components/Forbidden.tsx
+++ components/Forbidden.tsx
@@ -1,30 +1,30 @@
 import React from "react";
 import Link from "next/link";
 const Forbidden = ({ placeholder }: any) => {
+  const srcArray = [
+    "https://giphy.com/embed/VphQlWcd8xUThBQ9R2/video",
+    "https://giphy.com/embed/tSPd9mtiuicMwuovjN",
+    "https://giphy.com/embed/cdqwNuGaUg3Qs",
+    "https://giphy.com/embed/yqKLjY7itedDeO0Dyn/video",
+    "https://giphy.com/embed/szEWryz4gtspG",
+  ];
+  const src = Math.floor(Math.random() * srcArray.length);
   return (
     <div className="flex flex-col items-center my-10">
       <iframe
-        src="https://giphy.com/embed/tSPd9mtiuicMwuovjN"
+        src={srcArray[src]}
         width="480"
         height="270"
-        className="giphy-embed w-[300px] sm:w-full px-5"
+        className="giphy-embed w-[300px] sm:w-[800px] px-5"
       ></iframe>
 
-      <a
-        href="https://giphy.com/gifs/cartoonnetwork-cartoon-network-adventure-time-fionna-and-cake-tSPd9mtiuicMwuovjN"
-        className="text-white underline text-sm hidden sm:block mb-5"
-      >
-        via GIPHY
-      </a>
-
       <Link
-        href="/"
-        className=" text-center text-md  sm: text-lg md:tex-xl lg:text-2xl text-primaryAccent font-Montserrat hover:text-primaryAccentHover"
+        href="/auth/login"
+        className=" text-center text-md mt-10  sm: text-lg md:tex-xl lg:text-2xl text-primaryAccent font-Montserrat hover:text-primaryAccentHover"
       >
         {placeholder}
       </Link>
     </div>
   );
 };
-
 export default Forbidden;

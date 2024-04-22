ce97351b0eddfdaa3a68dc643fab8a6aa5946242
RobertoCiurea <robertociureaionut@gmail.com>
2024-04-22 17:14:29 +0300

    add comment action

---
+++ actions/addBlogPostComment.ts
@@ -0,0 +1,56 @@
+"use server";
+import { PrismaClient } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+const prisma = new PrismaClient();
+
+export const addBlogPostComment = async (
+  prevState: any,
+  formData: FormData
+) => {
+  try {
+    const author = formData.get("author");
+    const content = formData.get("comment");
+    const blogPostId = formData.get("blogPostId");
+    if (!author) {
+      return {
+        message: "You must be authenticated to add a comment",
+        status: 401,
+      };
+    }
+    if (!content) {
+      return {
+        message: "You must write something in order to add a comment",
+        status: 400,
+      };
+    }
+    //assure that content of comment is a string
+    const contentString = content as string;
+    if (contentString.length < 3) {
+      return {
+        message: "You need at least 3 characters in order to add a comment",
+        status: 400,
+      };
+    }
+    const comment = await prisma.comment.create({
+      data: {
+        author: author as string,
+        content: content as string,
+        createdAt: new Date(),
+        blogPostId: blogPostId as string,
+      },
+    });
+    revalidatePath("blog-posts/[id]");
+    if (comment) {
+      return {
+        message: "Comment added successfully",
+        status: 200,
+      };
+    }
+  } catch (error) {
+    console.log(error);
+    return {
+      message: "Something went wrong",
+      status: 500,
+    };
+  }
+};

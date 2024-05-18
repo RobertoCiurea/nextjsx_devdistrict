878ea0d42ae16737b2b6ff500141766f5888610b
RobertoCiurea <robertociureaionut@gmail.com>
2024-05-18 15:50:41 +0300

    modified for admin panel

--- components/ReportCard.tsx
+++ components/ReportCard.tsx
@@ -1,4 +1,11 @@
+"use session";
 import React from "react";
+import { useSession } from "next-auth/react";
+import { useFormStatus } from "react-dom";
+//actions
+import { deleteReport } from "@/actions/deleteReport";
+import { modifyReportStatus } from "@/actions/modifyReportStatus";
+import Button from "./Button";
 type ReportCardType = {
   id: string;
   userId: string;
@@ -17,18 +24,64 @@ const ReportCard = ({
   status,
   createdAt,
 }: ReportCardType) => {
+  const session = useSession();
   const date = createdAt.toLocaleString().split(" ");
   return (
-    <div className="flex flex-col px-5 py-2 bg-backgroundAccent text-primaryGray rounded-lg gap-3">
+    <div className="flex flex-col justify-between px-5 py-2 bg-backgroundAccent text-primaryGray rounded-lg gap-3">
+      {session.data?.user?.name === "ADMIN" ? (
+        <div className="flex justify-between">
+          <h1>
+            User id:<span className="font-bold text-white"> {id}</span>
+          </h1>
+          <form action={deleteReport}>
+            <input type="hidden" name="reportId" defaultValue={id} />
+            <FormButton
+              title="Delete report"
+              styles="bg-red-600 hover:bg-red-700"
+            />
+          </form>
+        </div>
+      ) : (
+        <div className="flex justify-end">
+          <form action={deleteReport}>
+            <input type="hidden" name="reportId" defaultValue={id} />
+            <FormButton
+              title="Delete report"
+              styles="bg-red-600 hover:bg-red-700"
+            />
+          </form>
+        </div>
+      )}
       <h1 className="sm:text-xl text-white">{title}</h1>
       <p>{description}</p>
       {/*Bottom section */}
       <div className="flex justify-between items-center text-white">
-        <h1>{date[0]}</h1>
+        <h1>{date}</h1>
         <h1>{status}</h1>
       </div>
+      {/*Update status of report as ADMIN */}
+      {session.data?.user?.name === "ADMIN" && status === "PENDING" && (
+        <form action={modifyReportStatus}>
+          <input type="hidden" name="reportId" defaultValue={id} />
+          <FormButton
+            title="Mark as resolved"
+            styles="bg-primaryAccent hover:bg-primaryAccentHover py-1"
+          />
+        </form>
+      )}
     </div>
   );
 };
 
 export default ReportCard;
+const FormButton = ({ title, styles }: { title: string; styles: string }) => {
+  const { pending } = useFormStatus();
+  return (
+    <Button
+      title={pending ? "Loading..." : title}
+      styles=""
+      buttonType={"submit"}
+      buttonStyles={` ${styles}  transition-colors shadow-xl text-white px-2 rounded-lg`}
+    />
+  );
+};

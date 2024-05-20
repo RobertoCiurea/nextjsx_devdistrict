1c4549e7e24e1e996d1cde9885749b872484f024
RobertoCiurea <robertociureaionut@gmail.com>
2024-05-20 18:19:20 +0300

    added question page

---
+++ app/questions/[...id]/page.tsx
@@ -0,0 +1,94 @@
+import React from "react";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+import Link from "next/link";
+import { getSession } from "@/app/utils/getSession";
+import EditorComponent from "@/components/EditorComponent";
+import SolutionForm from "@/components/SolutionForm";
+import Solution from "@/components/Solution";
+const page = async ({ params }: { params: { id: string | any } }) => {
+  const questionId = params.id[0];
+  const session = await getSession();
+  //query the question by its id from database and include also the solutions and the replies for solutions
+  const question = await prisma.question.findUnique({
+    where: {
+      id: questionId,
+    },
+    include: {
+      solutions: {
+        include: {
+          replies: true,
+          votes: true,
+        },
+      },
+    },
+  });
+
+  return (
+    <section className="flex justify-center gap-10 items-center flex-col mx-2 text-white font-Montserrat">
+      {/*Top section */}
+      <div className="flex flex-col gap-3 sm:flex-row w-full justify-evenly">
+        {/*Left section */}
+        <div className="flex flex-col gap-1">
+          <h1 className="tetx-xl sm:text-2xl">{question?.title}</h1>
+          {/*Statistic data */}
+          <div className="flex justify-around font-Raleway text-primaryGray">
+            <span>Created at: {question?.createdAt.toLocaleDateString()}</span>
+            <span className="text-sm">Views: {question?.viewsCounter}</span>
+          </div>
+        </div>
+        <h1 className="sm:text-lg">
+          By:{" "}
+          <Link
+            href={`/account/${question?.author}`}
+            className="underline font-Raleway hover:text-primaryAccentHover transition-colors"
+          >
+            {question?.author}
+          </Link>
+        </h1>
+      </div>
+      {/*Content - middle section */}
+      <div className="flex flex-col mx-5">
+        <p className="font-Raleway">{question?.description}</p>
+        {/*Editor */}
+        {question?.code !== "" && (
+          <EditorComponent
+            language={question?.language as string}
+            code={question?.code as string}
+          />
+        )}
+      </div>
+      {/*Answers */}
+      <div className="flex flex-col">
+        <h1 className="text-xl text-white">
+          {question?.answersCounter} Answers
+        </h1>
+        {(question?.solutions?.length as number) > 0 &&
+          question?.solutions?.map((solution) => (
+            <Solution
+              key={solution.id}
+              votes={solution.votesCounter}
+              id={solution.id}
+              author={solution.author}
+              language={solution.language}
+              code={solution.code}
+              content={solution.content}
+              createdAt={solution.createdAt}
+              userId={session?.user.id as string}
+            />
+          ))}
+      </div>
+      {/*Soltuion area */}
+      {session?.user && (
+        <SolutionForm
+          questionId={question?.id as string}
+          userId={session?.user.id as string}
+          author={session?.user.name as string}
+          language={question?.language as string}
+        />
+      )}
+    </section>
+  );
+};
+
+export default page;

223c66b39706f91cecd75e6492f87ae16bc02d0b
RobertoCiurea <robertociureaionut@gmail.com>
2024-05-21 21:39:19 +0300

    add one view to question by visiting it

---
+++ actions/viewQuestion.ts
@@ -0,0 +1,33 @@
+"use server";
+import { PrismaClient } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+const prisma = new PrismaClient();
+let view: any;
+let questionId: string | undefined;
+export const viewQuestion = async (formData: FormData) => {
+  try {
+    questionId = formData.get("questionId")?.toString();
+    view = await prisma.question.update({
+      where: {
+        id: questionId,
+      },
+      data: {
+        viewsCounter: {
+          increment: 1,
+        },
+      },
+    });
+
+    revalidatePath("/", "page");
+    revalidatePath("/admin-panel", "page");
+    revalidatePath("/account/[name]", "page");
+  } catch (error) {
+    console.log(error);
+  } finally {
+    if (view) {
+      console.log("redirected nigga");
+      redirect(`questions/${questionId}`);
+    }
+  }
+};

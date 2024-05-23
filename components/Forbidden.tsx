6bba54925747da755fe8eec1403933cd1611d2b0
RobertoCiurea <robertociureaionut@gmail.com>
2024-05-23 16:25:45 +0300

    fixed feedback problem

--- components/Forbidden.tsx
+++ components/Forbidden.tsx
@@ -1,6 +1,8 @@
+"use client";
 import React from "react";
 import Link from "next/link";
-const Forbidden = ({ placeholder }: any) => {
+import { useSession } from "next-auth/react";
+const Forbidden = ({ placeholder }: { placeholder: string }) => {
   const srcArray = [
     "https://giphy.com/embed/VphQlWcd8xUThBQ9R2/video",
     "https://giphy.com/embed/tSPd9mtiuicMwuovjN",
@@ -9,10 +11,11 @@ const Forbidden = ({ placeholder }: any) => {
     "https://giphy.com/embed/szEWryz4gtspG",
   ];
   const src = Math.floor(Math.random() * srcArray.length);
+  const session = useSession();
   return (
     <div className="flex flex-col gap-2 items-center my-10">
       <h1 className="text-xl sm:text-2xl text-white font-bold">
-        You must be logged in order to access this page
+        {placeholder ? placeholder : "You don't have access to this page"}
       </h1>
 
       <iframe
@@ -21,12 +24,14 @@ const Forbidden = ({ placeholder }: any) => {
         height="270"
         className="giphy-embed w-[300px] sm:w-[800px] px-5"
       ></iframe>
-      <Link
-        href={"/auth/login"}
-        className="text-primaryAccent underline sm:text-lg hover:text-primaryAccentHover transition-colors"
-      >
-        Log in
-      </Link>
+      {session.data?.user === null && (
+        <Link
+          href={"/auth/login"}
+          className="text-primaryAccent underline sm:text-lg hover:text-primaryAccentHover transition-colors"
+        >
+          Log in
+        </Link>
+      )}
     </div>
   );
 };

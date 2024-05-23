105796987fbd03d40e818c404ea881841c307d2c
RobertoCiurea <robertociureaionut@gmail.com>
2024-05-23 20:53:23 +0300

    production test #3

--- app/questions/[...id]/page.tsx
+++ app/questions/[...id]/page.tsx
@@ -73,73 +73,72 @@ const page = async ({ params }: { params: { id: string | any } }) => {
     },
   });
 
-  // return (
-  //   <section className="flex justify-center gap-10 items-center flex-col mx-2 text-white font-Montserrat">
-  //     {/*Top section */}
-  //     <div className="flex flex-col gap-3 sm:flex-row w-full justify-evenly">
-  //       {/*Left section */}
-  //       <div className="flex flex-col gap-1">
-  //         <h1 className="tetx-xl sm:text-2xl">{question?.title}</h1>
-  //         {/*Statistic data */}
-  //         <div className="flex justify-around font-Raleway text-primaryGray">
-  //           <span>Created at: {question?.createdAt.toLocaleDateString()}</span>
-  //           <span className="text-sm">Views: {question?.viewsCounter}</span>
-  //         </div>
-  //       </div>
-  //       <h1 className="sm:text-lg">
-  //         By:{" "}
-  //         <Link
-  //           href={`/account/${question?.author}`}
-  //           className="underline font-Raleway hover:text-primaryAccentHover transition-colors"
-  //         >
-  //           {question?.author}
-  //         </Link>
-  //       </h1>
-  //     </div>
-  //     {/*Content - middle section */}
-  //     <div className="flex flex-col mx-5">
-  //       <p className="font-Raleway">{question?.description}</p>
-  //       {/*Editor */}
-  //       {question?.code !== "" && (
-  //         <EditorComponent
-  //           language={question?.language as string}
-  //           code={question?.code as string}
-  //         />
-  //       )}
-  //     </div>
-  //     {/*Answers */}
-  //     <div className="flex flex-col">
-  //       <h1 className="text-xl text-white">
-  //         {question?.answersCounter} Answers
-  //       </h1>
-  //       {(question?.solutions?.length as number) > 0 &&
-  //         question?.solutions?.map((solution) => (
-  //           <Solution
-  //             key={solution.id}
-  //             votes={solution.votesCounter}
-  //             id={solution.id}
-  //             author={solution.author}
-  //             language={solution.language}
-  //             code={solution.code}
-  //             content={solution.content}
-  //             createdAt={solution.createdAt}
-  //             userId={session?.user.id as string}
-  //             replies={solution.replies}
-  //           />
-  //         ))}
-  //     </div>
-  //     {/*Soltuion area */}
-  //     {session?.user && (
-  //       <SolutionForm
-  //         questionId={question?.id as string}
-  //         userId={session?.user.id as string}
-  //         author={session?.user.name as string}
-  //         language={question?.language as string}
-  //       />
-  //     )}
-  //   </section>
-  // );
-  return <h1>Solution with id{params.id}</h1>;
+  return (
+    <section className="flex justify-center gap-10 items-center flex-col mx-2 text-white font-Montserrat">
+      {/*Top section */}
+      <div className="flex flex-col gap-3 sm:flex-row w-full justify-evenly">
+        {/*Left section */}
+        <div className="flex flex-col gap-1">
+          <h1 className="tetx-xl sm:text-2xl">{question?.title}</h1>
+          {/*Statistic data */}
+          <div className="flex justify-around font-Raleway text-primaryGray">
+            <span>Created at: {question?.createdAt.toLocaleDateString()}</span>
+            <span className="text-sm">Views: {question?.viewsCounter}</span>
+          </div>
+        </div>
+        <h1 className="sm:text-lg">
+          By:{" "}
+          <Link
+            href={`/account/${question?.author}`}
+            className="underline font-Raleway hover:text-primaryAccentHover transition-colors"
+          >
+            {question?.author}
+          </Link>
+        </h1>
+      </div>
+      {/*Content - middle section */}
+      <div className="flex flex-col mx-5">
+        <p className="font-Raleway">{question?.description}</p>
+        {/*Editor */}
+        {question?.code !== "" && (
+          <EditorComponent
+            language={question?.language as string}
+            code={question?.code as string}
+          />
+        )}
+      </div>
+      {/*Answers */}
+      <div className="flex flex-col">
+        <h1 className="text-xl text-white">
+          {question?.answersCounter} Answers
+        </h1>
+        {/* {(question?.solutions?.length as number) > 0 &&
+          question?.solutions?.map((solution) => (
+            <Solution
+              key={solution.id}
+              votes={solution.votesCounter}
+              id={solution.id}
+              author={solution.author}
+              language={solution.language}
+              code={solution.code}
+              content={solution.content}
+              createdAt={solution.createdAt}
+              userId={session?.user.id as string}
+              replies={solution.replies}
+            />
+          ))} */}
+      </div>
+      {/*Soltuion area */}
+      {session?.user && (
+        <SolutionForm
+          questionId={question?.id as string}
+          userId={session?.user.id as string}
+          author={session?.user.name as string}
+          language={question?.language as string}
+        />
+      )}
+    </section>
+  );
 };
 
 export default page;

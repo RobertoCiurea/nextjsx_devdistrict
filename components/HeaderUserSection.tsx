3ec2e4ffcd37ad7bb32e63ce27c2b48e27e1cbd1
RobertoCiurea <robertociureaionut@gmail.com>
2024-05-23 16:57:45 +0300

    small fix with my account and sign out component

--- components/HeaderUserSection.tsx
+++ components/HeaderUserSection.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import Link from "next/link";
 import HamburgerMenu from "./HamburgerMenu";
-import SignOutButton from "./SIgnOutButton";
+import { signOut } from "next-auth/react";
 import Image from "next/image";
 import { useSession } from "next-auth/react";
 
@@ -13,23 +13,27 @@ const HeaderUserSection = () => {
       <div className="block md:hidden">
         <HamburgerMenu />
       </div>
-      {session?.data?.user && (
-        <div className="hidden md:flex items-center gap-5 flex-shrink">
-          <Link
-            href="/account/[name]"
-            as={`/account/${session.data.user?.name}`}
-          >
-            <Image
-              className="hidden md:block rounded-full"
-              src={session.data.user.image as string}
-              alt="Avatar Image"
-              width={70}
-              height={70}
-            />
-          </Link>
-          <SignOutButton styles="md:p-3" />
-        </div>
-      )}
+
+      <div className="hidden md:flex items-center gap-5 flex-shrink">
+        <Link
+          href="/account/[name]"
+          as={`/account/${session?.data?.user?.name}`}
+        >
+          <Image
+            className="hidden md:block rounded-full"
+            src={session?.data?.user?.image as string}
+            alt="Avatar Image"
+            width={70}
+            height={70}
+          />
+        </Link>
+        <button
+          className="font-Raleway text-lg text-center  text-primaryAccent rounded-full font-bold bg-white shadow-lg shadow-white/35 hover:bg-slate-200 hover:shadow-white/30 md:p-3"
+          onClick={() => signOut({ callbackUrl: "/auth/login" })}
+        >
+          Sign out
+        </button>
+      </div>
     </>
   );
 };
